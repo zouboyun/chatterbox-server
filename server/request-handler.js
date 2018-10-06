e85794ed7c4c61c315d0be4cedc03ae4e41161ed
Hack Reactor Student <student@hackreactor.com>
2018-10-06 10:59:57 -0700

    COMPLETED MIN REQUIREMENTS

--- server/request-handler.js
+++ server/request-handler.js
@@ -12,7 +12,51 @@ this file and include it in basic-server.js so that it actually works.
 
 **************************************************************/
 
+var messages = [{
+          username: 'Mel Brooks',
+          text: 'It\'s good to be the king',
+          roomname: 'lobby'
+        }];
+
 var requestHandler = function(request, response) {
+  
+  console.log('Serving request type ' + request.method + ' for url ' + request.url);
+  
+  var statusCode = 200;
+  var headers = defaultCorsHeaders;
+  headers['Content-Type'] = 'application/json';
+  
+  if (request.method === "GET") {
+    if (request.url.includes('/classes/messages')) {
+      response.writeHead(statusCode, headers);
+      response.end(JSON.stringify({results: messages}))    
+    } else {
+      statusCode = 404;
+      response.writeHead(statusCode, headers);
+      response.end('OOPS 404!') 
+    }
+  
+  } else if (request.method === "POST") {
+    statusCode = 201;
+    let body = [];
+    request.on('data', (chunk) => {
+      body.push(chunk);
+    }).on('end', () => {
+      body = Buffer.concat(body).toString();
+      // at this point, `body` has the entire request body stored in it as a string
+      body = JSON.parse(body);
+      body.createdAt = new Date();
+      messages.unshift(body);
+      response.writeHead(statusCode, headers);
+      response.end(JSON.stringify(body));
+    });
+    
+  } else if (request.method === "OPTIONS") {
+    headers['Allow'] = 'GET, POST, PUT, DELETE, OPTIONS';
+    response.writeHead(statusCode, headers);
+    response.end(JSON.stringify({results: messages})) 
+  };
+  
   // Request and Response come from node's http module.
   //
   // They include information about both the incoming request, such as
@@ -27,23 +71,23 @@ var requestHandler = function(request, response) {
   // Adding more logging to your server can be an easy way to get passive
   // debugging help, but you should always be careful about leaving stray
   // console.logs in your code.
-  console.log('Serving request type ' + request.method + ' for url ' + request.url);
+
 
   // The outgoing status.
-  var statusCode = 200;
+  //var statusCode = 200;
 
   // See the note below about CORS headers.
-  var headers = defaultCorsHeaders;
+  //var headers = defaultCorsHeaders;
 
   // Tell the client we are sending them plain text.
   //
   // You will need to change this if you are sending something
   // other than plain text, like JSON or HTML.
-  headers['Content-Type'] = 'text/plain';
+  //headers['Content-Type'] = 'text/plain';
 
   // .writeHead() writes to the request line and headers of the response,
   // which includes the status and all headers.
-  response.writeHead(statusCode, headers);
+  //response.writeHead(statusCode, headers);
 
   // Make sure to always call response.end() - Node may not send
   // anything back to the client until you do. The string you pass to
@@ -52,7 +96,7 @@ var requestHandler = function(request, response) {
   //
   // Calling .end "flushes" the response's internal buffer, forcing
   // node to actually send all the data over to the client.
-  response.end('Hello, World!');
+  //response.end('Hello, World!');
 };
 
 // These headers will allow Cross-Origin Resource Sharing (CORS).
@@ -67,7 +111,8 @@ var requestHandler = function(request, response) {
 var defaultCorsHeaders = {
   'access-control-allow-origin': '*',
   'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
-  'access-control-allow-headers': 'content-type, accept',
+  'access-control-allow-headers': 'content-type, accept, X-Parse-Application-Id, X-Parse-REST-API-Key',
   'access-control-max-age': 10 // Seconds.
 };
 
+module.exports.requestHandler = requestHandler;
